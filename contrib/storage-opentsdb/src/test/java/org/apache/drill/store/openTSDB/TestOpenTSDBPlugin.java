b58fda2dd19f03ae8d1b0534dccb50b6a491d28d
Vlad Storona <vstorona@cybervisiontech.com>
2017-03-21 12:18:17 +0000

    Transform javadoc headers to comments

--- contrib/storage-opentsdb/src/test/java/org/apache/drill/store/openTSDB/TestOpenTSDBPlugin.java
+++ contrib/storage-opentsdb/src/test/java/org/apache/drill/store/openTSDB/TestOpenTSDBPlugin.java
@@ -1,13 +1,14 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p>
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -60,9 +61,9 @@ public class TestOpenTSDBPlugin extends BaseTestQuery {
 
     stubFor(get(urlEqualTo("/api/query?start=47y-ago&m=sum:warp.spee"))
         .willReturn(aResponse()
-                .withStatus(400)
-                .withHeader("Content-Type", "application/json")
-            ));
+            .withStatus(400)
+            .withHeader("Content-Type", "application/json")
+        ));
 
     stubFor(get(urlEqualTo("/api/query?start=47y-ago&m=sum:warp.speed.test"))
         .willReturn(aResponse()
