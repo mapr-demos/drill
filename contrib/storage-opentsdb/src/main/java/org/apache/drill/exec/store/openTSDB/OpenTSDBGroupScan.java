bb35a8f1e272ce1b67d65f613163ef8f431a4d04
Vlad Storona <vstorona@cybervisiontech.com>
2017-11-02 17:23:12 +0200

    Reworked GroupScan

--- contrib/storage-opentsdb/src/main/java/org/apache/drill/exec/store/openTSDB/OpenTSDBGroupScan.java
+++ contrib/storage-opentsdb/src/main/java/org/apache/drill/exec/store/openTSDB/OpenTSDBGroupScan.java
@@ -23,12 +23,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.apache.drill.common.exceptions.ExecutionSetupException;
 import org.apache.drill.common.expression.SchemaPath;
-import org.apache.drill.exec.physical.EndpointAffinity;
 import org.apache.drill.exec.physical.base.AbstractGroupScan;
 import org.apache.drill.exec.physical.base.GroupScan;
 import org.apache.drill.exec.physical.base.PhysicalOperator;
@@ -38,17 +35,10 @@ import org.apache.drill.exec.store.StoragePluginRegistry;
 import org.apache.drill.exec.store.openTSDB.OpenTSDBSubScan.OpenTSDBSubScanSpec;
 import org.apache.drill.exec.store.openTSDB.client.services.ServiceImpl;
 import org.apache.drill.exec.store.openTSDB.dto.MetricDTO;
-import org.apache.drill.exec.store.schedule.AffinityCreator;
-import org.apache.drill.exec.store.schedule.AssignmentCreator;
-import org.apache.drill.exec.store.schedule.CompleteWork;
-import org.apache.drill.exec.store.schedule.EndpointByteMap;
-import org.apache.drill.exec.store.schedule.EndpointByteMapImpl;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 import static jdk.nashorn.internal.ir.debug.ObjectSizeCalculator.getObjectSize;
@@ -57,16 +47,11 @@ import static org.apache.drill.exec.store.openTSDB.Util.parseFromRowData;
 @JsonTypeName("openTSDB-scan")
 public class OpenTSDBGroupScan extends AbstractGroupScan {
 
-  private static final long DEFAULT_TABLET_SIZE = 1000;
-
   private OpenTSDBStoragePluginConfig storagePluginConfig;
   private OpenTSDBScanSpec openTSDBScanSpec;
   private OpenTSDBStoragePlugin storagePlugin;
 
-  private ListMultimap<Integer, OpenTSDBWork> assignments;
   private List<SchemaPath> columns;
-  private List<OpenTSDBWork> openTSDBWorkList = Lists.newArrayList();
-  private List<EndpointAffinity> affinities;
 
   @JsonCreator
   public OpenTSDBGroupScan(@JsonProperty("openTSDBScanSpec") OpenTSDBScanSpec openTSDBScanSpec,
@@ -83,7 +68,6 @@ public class OpenTSDBGroupScan extends AbstractGroupScan {
     this.storagePluginConfig = storagePlugin.getConfig();
     this.openTSDBScanSpec = scanSpec;
     this.columns = columns == null || columns.size() == 0 ? ALL_COLUMNS : columns;
-    init();
   }
 
   /**
@@ -97,36 +81,15 @@ public class OpenTSDBGroupScan extends AbstractGroupScan {
     this.openTSDBScanSpec = that.openTSDBScanSpec;
     this.storagePlugin = that.storagePlugin;
     this.storagePluginConfig = that.storagePluginConfig;
-    this.openTSDBWorkList = that.openTSDBWorkList;
-    this.assignments = that.assignments;
-    init();
-  }
-
-  private void init() {
-    Collection<DrillbitEndpoint> endpoints = storagePlugin.getContext().getBits();
-    Map<String, DrillbitEndpoint> endpointMap = Maps.newHashMap();
-
-    for (DrillbitEndpoint endpoint : endpoints) {
-      endpointMap.put(endpoint.getAddress(), endpoint);
-    }
-  }
-
-  @Override
-  public List<EndpointAffinity> getOperatorAffinity() {
-    if (affinities == null) {
-      affinities = AffinityCreator.getAffinityMap(openTSDBWorkList);
-    }
-    return affinities;
   }
 
   @Override
   public int getMaxParallelizationWidth() {
-    return openTSDBWorkList.size();
+    return 1;
   }
 
   @Override
   public void applyAssignments(List<DrillbitEndpoint> incomingEndpoints) {
-    assignments = AssignmentCreator.getMappings(incomingEndpoints, openTSDBWorkList);
   }
 
   @Override
@@ -197,44 +160,7 @@ public class OpenTSDBGroupScan extends AbstractGroupScan {
 
   @Override
   public String toString() {
-    return "MongoGroupScan [MongoScanSpec=" + openTSDBScanSpec + ", columns=" + columns
+    return "OpenTSDBGroupScan [OpenTSDBScanSpec=" + openTSDBScanSpec + ", columns=" + columns
             + "]";
-  }
-
-  private static class OpenTSDBWork implements CompleteWork {
-
-    private EndpointByteMapImpl byteMap = new EndpointByteMapImpl();
-
-    @Override
-    public long getTotalBytes() {
-      return DEFAULT_TABLET_SIZE;
-    }
-
-    @Override
-    public EndpointByteMap getByteMap() {
-      return byteMap;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || getClass() != o.getClass()) {
-        return false;
-      }
-      OpenTSDBWork that = (OpenTSDBWork) o;
-      return Objects.equals(byteMap, that.byteMap);
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(byteMap);
-    }
-
-    @Override
-    public int compareTo(CompleteWork o) {
-      return 0;
-    }
   }
 }
